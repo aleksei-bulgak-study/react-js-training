d7e0dd6069df3b3ce1ceaa66216b320efec7f77d
aleksei-bulgak-study <bulgak.aleksei@yandex.ru>
2020-08-20 16:18:35 +0300

    [ERMP][task4] move ternar function out of jsx

--- src/components/Common/HamburgerButton/HamburgerButton.jsx
+++ src/components/Common/HamburgerButton/HamburgerButton.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import './styles.css';
 
-const HamburgerButton = ({ onClick, visible, className }) => (
-  <button
-    type="button"
-    className={`hamburger ${className}`}
-    style={{ display: visible ? 'inline-block' : 'none' }}
-    onClick={onClick}
-  >
-    <div className="hamburger__dot" />
-  </button>
-);
+const HamburgerButton = ({ onClick, visible, className }) => {
+  const display = useMemo(() => (visible ? 'inline-block' : 'none'), [visible]);
+  return (
+    <button
+      type="button"
+      className={`hamburger ${className}`}
+      style={{ display }}
+      onClick={onClick}
+    >
+      <div className="hamburger__dot" />
+    </button>
+  );
+};
 
 HamburgerButton.propTypes = {
   onClick: PropTypes.func.isRequired,
